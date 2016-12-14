ca713e5d7069a11ca3212419696a4c3c837983dd
Mikhail Kanygin <kanygin@vector-best.ru>
2016-12-14 20:03:44 +0600

    Just to build with assembly signing.

--- Raspberry.IO.SerialPeripheralInterface/Properties/AssemblyInfo.cs
+++ Raspberry.IO.SerialPeripheralInterface/Properties/AssemblyInfo.cs
@@ -13,4 +13,11 @@ using System.Runtime.InteropServices;
 [assembly: Guid("5548a61b-6677-435f-be41-13aa4f05e994")]
 
 // Make the internal methods, classes etc. visible to the unit test project.
-[assembly: InternalsVisibleTo("Tests.Raspberry.IO.SerialPeripheralInterface")]
+[assembly: InternalsVisibleTo("Tests.Raspberry.IO.SerialPeripheralInterface" + ",PublicKey=" +
+    "00240000048000009400000006020000002400005253413100040000010001009327379061d646"+
+    "c6a73b789badc874bcdd90029614edabf77f29cf76e4dabd1d7a1567bec176a3de91afe8516470"+
+    "f8148e8be8cc2512de1ccd8a0c700101bd2df4ea95ba0358171c860c3027e19b214f3cf850b922"+
+    "037012851d9765ae5f5a8ea5e0f14012fe76ba2fab10f2577bab2c9a39c94db6a87aebd0e1b421"+
+    "b94630bd"
+)]
+
